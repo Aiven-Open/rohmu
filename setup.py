7102be03daea945793152e3be79886a01e19fed6
Kevin Michel <kevin.michel@aiven.io>
2022-04-27 14:07:44 +0200

    packaging: add setup.py
    This can be installed by using `pip install -e .`

---
+++ setup.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+# This file enables editable installs with `pip install --editable .`
+# https://setuptools.readthedocs.io/en/latest/setuptools.html#setup-cfg-only-projects
+
+import setuptools
+
+setuptools.setup()
