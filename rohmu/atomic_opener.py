2748e2ec92339ff5d01eeedd88520a6753dad3b7
Alan Franzoni <alan.franzoni@aiven.io>
2022-09-02 17:01:15 +0200

    Support for atomically creating a file with content
    Many functionalities in rohmu end up being broken if anything "bad"
    happens during a function call, since a file can be half-written.
    We cannot perform the usual write-to-temp-then-move-to-final in many
    places, since we've got no control on the original source directory,
    which could be used by client code for other purposes.
    
    So, in order not to need to change too many interfaces, we leverage
    Linux O_TMPFILE in order to create an anonymous temporary file in the
    same directory and hence same filesystem as the one that was requested,
    and we atomically create a real hardlink on the fs only if the operation
    completely succeeded.
    
    Implementation is taken from Linux open(2) manual about O_TMPFILE, almost
    verbatim.

---
+++ rohmu/atomic_opener.py
@@ -0,0 +1,115 @@
+from contextlib import contextmanager
+from pathlib import Path
+from typing import (
+    BinaryIO,
+    Callable,
+    cast,
+    ContextManager,
+    Generator,
+    Literal,
+    Optional,
+    overload,
+    TextIO,
+    TYPE_CHECKING,
+    Union,
+)
+
+import errno
+import os
+
+
+def _fd_close_quietly(fd: int) -> None:
+    try:
+        os.close(fd)
+    except OSError as e:
+        if e.errno == errno.EBADF:
+            # closed already
+            return
+        raise
+
+
+@overload
+def atomic_opener(
+    final_path: Path,
+    mode: Literal["wb"],
+    encoding: Optional[str] = None,
+    _fd_spy: Callable[[int], None] = lambda unused: None,
+    _after_link_hook: Callable[[], None] = lambda: None,
+) -> ContextManager[BinaryIO]:
+    ...
+
+
+@overload
+def atomic_opener(
+    final_path: Path,
+    mode: Literal["w"],
+    encoding: Optional[str] = None,
+    _fd_spy: Callable[[int], None] = lambda unused: None,
+    _after_link_hook: Callable[[], None] = lambda: None,
+) -> ContextManager[TextIO]:
+    ...
+
+
+if TYPE_CHECKING:
+    # necessary for testing or tests will fail type checking
+    @overload
+    def atomic_opener(
+        final_path: Path,
+        mode: Literal["somethingrandomw"],
+        encoding: Optional[str] = None,
+        _fd_spy: Callable[[int], None] = lambda unused: None,
+        _after_link_hook: Callable[[], None] = lambda: None,
+    ) -> ContextManager[TextIO]:
+        ...
+
+
+def atomic_opener(
+    final_path: Path,
+    mode: str,
+    encoding: Optional[str] = None,
+    _fd_spy: Callable[[int], None] = lambda unused: None,
+    _after_link_hook: Callable[[], None] = lambda: None,
+) -> ContextManager[Union[TextIO, BinaryIO]]:
+    return _atomic_opener(final_path, mode, encoding, _fd_spy, _after_link_hook)
+
+
+@contextmanager
+def _atomic_opener(
+    final_path: Path,
+    mode: str,
+    encoding: Optional[str] = None,
+    _fd_spy: Callable[[int], None] = lambda unused: None,
+    _after_link_hook: Callable[[], None] = lambda: None,
+) -> Generator[Union[TextIO, BinaryIO], None, None]:
+    """
+    Creates a file object for writing which will only appear on the filesystem if the context manager succeeds.
+
+    It's designed to prevent partial writes (file will either be available with full content, or will be unavailable)
+    and the need for manual cleanup.
+    """
+    parent_dir = final_path.parent
+    if not parent_dir.exists():
+        raise IOError(f"Parent directory '{parent_dir}' must exist but is missing")
+    if "w" not in mode:
+        raise ValueError("Write mode must be used to make actual sense")
+
+    fd = os.open(str(parent_dir), os.O_TMPFILE | os.O_RDWR, 0o600)
+    _fd_spy(fd)
+    try:
+        file_obj = os.fdopen(fd, mode=mode, encoding=encoding)
+    except Exception:  # pylint: disable=broad-except
+        # when passing wrong mode, os.fdopen won't close input fd. When passing wrong encoding, it will.
+        _fd_close_quietly(fd)
+        raise
+
+    try:
+        if "b" in mode:
+            yield cast(BinaryIO, file_obj)
+        else:
+            yield cast(TextIO, file_obj)
+        file_obj.flush()
+        path = "/proc/self/fd/{0}".format(fd)
+        os.link(path, str(final_path), src_dir_fd=0, follow_symlinks=True)
+        _after_link_hook()
+    finally:
+        file_obj.close()
---
+++ test/test_atomic_opener.py
@@ -0,0 +1,133 @@
+from pathlib import Path
+from rohmu.atomic_opener import atomic_opener
+
+import errno
+import os
+import pytest
+import time
+
+
+def _verify_file_not_created_and_dir_not_polluted(output_file: Path):
+    assert os.listdir(output_file.parent) == []
+    assert not output_file.exists()
+
+
+def test_file_is_atomically_created_only_after_function_execution_is_over(tmp_path: Path):
+    data_block = "x" * 100_000_000
+    # manually tested with block_count of 1000 but it seems overkill to do it every time and it can hog
+    # the testing infra quite a bit.
+    # block_count = 1000
+    block_count = 1
+    size = len(data_block)
+
+    output_file = tmp_path / "something"
+    try:
+        _verify_file_not_created_and_dir_not_polluted(output_file)
+        with atomic_opener(output_file, mode="w") as f:
+            inode_inside = os.stat(f.fileno()).st_ino
+            _verify_file_not_created_and_dir_not_polluted(output_file)
+            for unused_counter in range(block_count):
+                f.write(data_block)
+                f.flush()
+                _verify_file_not_created_and_dir_not_polluted(output_file)
+            timestamp_before_exiting = time.monotonic()
+
+        # this must be super-fast since atomic move is taking place. Makes more sense with
+        # a large - 100ish - block count
+        assert (time.monotonic() - timestamp_before_exiting) < 0.1
+        assert output_file.exists()
+        assert len(os.listdir(output_file.parent)) == 1
+        assert output_file.stat().st_size == size * block_count
+        assert output_file.stat().st_ino == inode_inside
+    finally:
+        # the file can be big, we want to delete it ASAP
+        try:
+            output_file.unlink()
+        except FileNotFoundError:
+            pass
+
+
+def test_file_is_never_created_if_function_breaks(tmp_path: Path):
+    output_file = tmp_path / "something"
+
+    try:
+        _verify_file_not_created_and_dir_not_polluted(output_file)
+        with atomic_opener(output_file, mode="w") as f:
+            _verify_file_not_created_and_dir_not_polluted(output_file)
+            f.write("aaaaaaaaaa")
+            f.flush()
+            _verify_file_not_created_and_dir_not_polluted(output_file)
+            time.sleep(1.0)
+            _verify_file_not_created_and_dir_not_polluted(output_file)
+            raise ValueError("crash")
+        pytest.fail("codepath should never be taken")
+    except ValueError:
+        _verify_file_not_created_and_dir_not_polluted(output_file)
+
+
+def test_file_is_fully_written_if_visible(tmp_path: Path):
+    output_file = tmp_path / "something"
+
+    def linkhook():
+        assert output_file.exists()
+        assert output_file.read_text() == "aaaaaaaaaa"
+
+    try:
+        _verify_file_not_created_and_dir_not_polluted(output_file)
+        with atomic_opener(output_file, mode="w", _after_link_hook=linkhook) as f:
+            _verify_file_not_created_and_dir_not_polluted(output_file)
+            f.write("aaaaaaaaaa")
+            _verify_file_not_created_and_dir_not_polluted(output_file)
+            time.sleep(1.0)
+            _verify_file_not_created_and_dir_not_polluted(output_file)
+
+    except ValueError:
+        _verify_file_not_created_and_dir_not_polluted(output_file)
+
+
+def test_open_for_writing_text_opens_proper_encoding_file(tmp_path: Path):
+    final_path = tmp_path / "file"
+    with atomic_opener(final_path, encoding="iso-8859-1", mode="w") as f:
+        f.write("à")
+    assert final_path.read_bytes() == b"\xe0"
+
+
+def test_open_for_writing_bytes_properly_writes_bytes(tmp_path: Path):
+    final_path = tmp_path / "file"
+    with atomic_opener(final_path, mode="wb") as f:
+        f.write(b"\xe0")
+    assert final_path.read_text("iso-8859-1") == "à"
+
+
+def test_no_fd_leak_if_fdopen_fails_because_of_wrong_encoding(tmp_path: Path):
+    final_path = tmp_path / "file"
+    opened_fd: list[int] = []
+    try:
+        with atomic_opener(final_path, mode="w", encoding="unknownencoding", _fd_spy=opened_fd.append):
+            pass
+        pytest.fail("should fail, encoding is wrong")
+    except LookupError:
+        try:
+            os.fstat(opened_fd[0])
+            pytest.fail("should fail, file descriptor must be invalid")
+        except OSError as e:
+            if e.errno != errno.EBADF:
+                raise
+            # descriptor is invalid, all ok
+
+
+def test_no_fd_leak_if_fdopen_fails_because_of_unknown_mode(tmp_path: Path):
+    final_path = tmp_path / "file"
+    opened_fd: list[int] = []
+    try:
+        with atomic_opener(final_path, mode="somethingrandomw", encoding="ascii", _fd_spy=opened_fd.append):
+            pass
+        pytest.fail("should fail, mode is wrong")
+    except ValueError:
+        try:
+            os.fstat(opened_fd[0])
+            pytest.fail("should fail, file descriptor must be invalid")
+        except OSError as e:
+            if e.errno != errno.EBADF:
+                raise
+            # descriptor is invalid, all ok
