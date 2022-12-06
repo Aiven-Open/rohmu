653502ae25077f01da6076d13f7e288dfb78dac4
Sumit <sumit.jamgade@aiven.io>
2022-12-06 20:00:38 +0100

    add missing test for google and fix getting size of uploaded data

--- test/test_object_storage_google.py
+++ test/test_object_storage_google.py
@@ -6,6 +6,27 @@ from tempfile import NamedTemporaryFile
 from unittest.mock import MagicMock, patch
 
 
+def test_store_file_from_memory() -> None:
+    notifier = MagicMock()
+    with patch("rohmu.object_storage.google.get_credentials") as _, patch(
+        "rohmu.object_storage.google.GoogleTransfer._upload"
+    ) as upload, patch("rohmu.object_storage.google.GoogleTransfer.get_or_create_bucket") as _:
+        transfer = GoogleTransfer(
+            project_id="test-project-id",
+            bucket_name="test-bucket",
+            notifier=notifier,
+        )
+        test_data = b"test-data"
+        metadata = {"Content-Length": len(test_data), "some-date": datetime(2022, 11, 15, 18, 30, 58, 486644)}
+        upload.return_value = {"size": len(test_data)}  # server reports the size of the uploaded object
+        transfer.store_file_from_memory("test_key1", memstring=test_data, metadata=metadata)
+
+        upload.assert_called()
+        notifier.object_created.assert_called_once_with(
+            key="test_key1", size=len(test_data), metadata={"Content-Length": "9", "some-date": "2022-11-15 18:30:58.486644"}
+        )
+
+
 def test_store_file_from_disk() -> None:
     notifier = MagicMock()
     with patch("rohmu.object_storage.google.get_credentials") as _, patch(
