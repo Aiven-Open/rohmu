b3600b7aec90a1cccee87050e3bd0de0e803ff14
Roman Inflianskas <rominf@aiven.io>
2023-08-18 07:56:04 +0300

    tests: replace constants with fixtures
    `CONSTANT_TEST_RSA_PUBLIC_KEY` and `CONSTANT_TEST_RSA_PRIVATE_KEY` are
    not special enough to make them constants and require separate file for
    them. Replace them with fixtures for consistency.

--- test/test_encryptor.py
+++ test/test_encryptor.py
@@ -2,7 +2,8 @@
 Copyright (c) 2015 Ohmu Ltd
 See LICENSE for details
 """
-from .base import CONSTANT_TEST_RSA_PRIVATE_KEY, CONSTANT_TEST_RSA_PUBLIC_KEY
+from __future__ import annotations
+
 from py.path import LocalPath  # type: ignore [import] # pylint: disable=import-error
 from rohmu import IO_BLOCK_SIZE
 from rohmu.encryptor import Decryptor, DecryptorFile, Encryptor, EncryptorFile, EncryptorStream
@@ -14,6 +15,43 @@ import os
 import pytest
 import random
 import tarfile
+import textwrap
+
+
+@pytest.fixture(name="rsa_public_key")
+def fixture_rsa_public_key() -> str:
+    return textwrap.dedent(
+        """\
+        -----BEGIN PUBLIC KEY-----
+        MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDQ9yu7rNmu0GFMYeQq9Jo2B3d9
+        hv5t4a+54TbbxpJlks8T27ipgsaIjqiQP7+uXNfU6UCzGFEHs9R5OELtO3Hq0Dn+
+        JGdxJlJ1prxVkvjCICCpiOkhc2ytmn3PWRuVf2VyeAddslEWHuXhZPptvIr593kF
+        lWN+9KPe+5bXS8of+wIDAQAB
+        -----END PUBLIC KEY-----"""
+    )
+
+
+@pytest.fixture(name="rsa_private_key")
+def fixture_rsa_private_key() -> str:
+    return textwrap.dedent(
+        """\
+        -----BEGIN PRIVATE KEY-----
+        MIICdwIBADANBgkqhkiG9w0BAQEFAASCAmEwggJdAgEAAoGBAND3K7us2a7QYUxh
+        5Cr0mjYHd32G/m3hr7nhNtvGkmWSzxPbuKmCxoiOqJA/v65c19TpQLMYUQez1Hk4
+        Qu07cerQOf4kZ3EmUnWmvFWS+MIgIKmI6SFzbK2afc9ZG5V/ZXJ4B12yURYe5eFk
+        +m28ivn3eQWVY370o977ltdLyh/7AgMBAAECgYEAkuAobRFhL+5ndTiZF1g1zCQT
+        aLepvbITwaL63B8GZz55LowRj5PL18/tyvYD1JqNWalZQIim67MKdOmGoRhXSF22
+        gUc6/SeqD27/9rsj8I+j0TrzLdTZwn88oX/gtndNutZuryCC/7KbJ8j18Jjn5qf9
+        ZboRKbEc7udxOb+RcYECQQD/ZLkxIvMSj0TxPUJcW4MTEsdeJHCSnQAhreIf2omi
+        hf4YwmuU3qnFA3ROje9jJe3LNtc0TK1kvAqfZwdpqyAdAkEA0XY4P1CPqycYvTxa
+        dxxWJnYA8K3g8Gs/Eo8wYKIciP+K70Q0GRP9Qlluk4vrA/wJJnTKCUl7YuAX6jDf
+        WdV09wJALGHXoQde0IHfTEEGEEDC9YSU6vJQMdpg1HmAS2LR+lFox+q5gWR0gk1I
+        YAJgcI191ovQOEF+/HuFKRBhhGZ9rQJAXOt13liNs15/sgshEq/mY997YUmxfNYG
+        v+P3kRa5U+kRKD14YxukARgNXrT2R+k54e5zZhVMADvrP//4RTDVVwJBAN5TV9p1
+        UPZXbydO8vZgPuo001KoEd9N3inq/yNcsHoF/h23Sdt/rcdfLMpCWuIYs/JAqE5K
+        nkMAHqg9PS372Cs=
+        -----END PRIVATE KEY-----"""
+    )
 
 
 @pytest.mark.parametrize(
@@ -23,14 +61,14 @@ import tarfile
         (b""),
     ),
 )
-def test_encryptor_decryptor(plaintext: bytes) -> None:
+def test_encryptor_decryptor(plaintext: bytes, rsa_private_key: str, rsa_public_key: str) -> None:
     for op in (None, "json"):
         if op == "json":
-            public_key = json.loads(json.dumps(CONSTANT_TEST_RSA_PUBLIC_KEY))
-            private_key = json.loads(json.dumps(CONSTANT_TEST_RSA_PRIVATE_KEY))
+            public_key = json.loads(json.dumps(rsa_public_key))
+            private_key = json.loads(json.dumps(rsa_private_key))
         else:
-            public_key = CONSTANT_TEST_RSA_PUBLIC_KEY
-            private_key = CONSTANT_TEST_RSA_PRIVATE_KEY
+            public_key = rsa_public_key
+            private_key = rsa_private_key
 
         encryptor = Encryptor(public_key)
         decryptor = Decryptor(private_key)
@@ -48,9 +86,9 @@ def test_encryptor_decryptor(plaintext: bytes) -> None:
         assert plaintext == decrypted
 
 
-def test_encryptor_stream() -> None:
+def test_encryptor_stream(rsa_private_key: str, rsa_public_key: str) -> None:
     plaintext = os.urandom(2 * 1024 * 1024)
-    encrypted_stream = EncryptorStream(io.BytesIO(plaintext), CONSTANT_TEST_RSA_PUBLIC_KEY)
+    encrypted_stream = EncryptorStream(io.BytesIO(plaintext), rsa_public_key)
     result_data = io.BytesIO()
     while True:
         bytes_requested = random.randrange(1, 12345)
@@ -66,28 +104,28 @@ def test_encryptor_stream() -> None:
         assert encrypted_stream.tell() == result_data.tell()
     assert result_data.tell() > 0
     result_data.seek(0)
-    decrypted = DecryptorFile(result_data, CONSTANT_TEST_RSA_PRIVATE_KEY).read()
+    decrypted = DecryptorFile(result_data, rsa_private_key).read()
     assert plaintext == decrypted
 
-    encrypted_stream = EncryptorStream(io.BytesIO(plaintext), CONSTANT_TEST_RSA_PUBLIC_KEY)
+    encrypted_stream = EncryptorStream(io.BytesIO(plaintext), rsa_public_key)
     result_data = io.BytesIO()
     result_data.write(encrypted_stream.read())
     result_data.seek(0)
-    decrypted = DecryptorFile(result_data, CONSTANT_TEST_RSA_PRIVATE_KEY).read()
+    decrypted = DecryptorFile(result_data, rsa_private_key).read()
     assert plaintext == decrypted
 
 
-def test_decryptorfile(tmpdir: LocalPath) -> None:
+def test_decryptorfile(tmpdir: LocalPath, rsa_public_key: str, rsa_private_key: str) -> None:
     # create a plaintext blob bigger than IO_BLOCK_SIZE
     plaintext1 = b"rvdmfki6iudmx8bb25tx1sozex3f4u0nm7uba4eibscgda0ckledcydz089qw1p1wer"
     repeat = int(1.5 * IO_BLOCK_SIZE / len(plaintext1))
     plaintext = repeat * plaintext1
-    encryptor = Encryptor(CONSTANT_TEST_RSA_PUBLIC_KEY)
+    encryptor = Encryptor(rsa_public_key)
     ciphertext = encryptor.update(plaintext) + encryptor.finalize()
     plain_fp = open(tmpdir.join("plain").strpath, mode="w+b")
     plain_fp.write(ciphertext)
     plain_fp.seek(0)
-    fp = DecryptorFile(plain_fp, CONSTANT_TEST_RSA_PRIVATE_KEY)  # pylint: disable=redefined-variable-type
+    fp = DecryptorFile(plain_fp, rsa_private_key)  # pylint: disable=redefined-variable-type
     assert fp.fileno() == plain_fp.fileno()
     assert fp.readable() is True
     assert fp.writable() is False
@@ -151,7 +189,7 @@ def test_decryptorfile(tmpdir: LocalPath) -> None:
         fp.truncate()
 
 
-def test_decryptorfile_for_tarfile(tmpdir: LocalPath) -> None:
+def test_decryptorfile_for_tarfile(tmpdir: LocalPath, rsa_public_key: str, rsa_private_key: str) -> None:
     testdata = b"file contents"
     data_tmp_name = tmpdir.join("plain.data").strpath
     with open(data_tmp_name, mode="wb") as data_tmp:
@@ -162,14 +200,14 @@ def test_decryptorfile_for_tarfile(tmpdir: LocalPath) -> None:
         tar.add(data_tmp_name, arcname="archived_content")
     plaintext = tar_data.getvalue()
 
-    encryptor = Encryptor(CONSTANT_TEST_RSA_PUBLIC_KEY)
+    encryptor = Encryptor(rsa_public_key)
     ciphertext = encryptor.update(plaintext) + encryptor.finalize()
     enc_tar_name = tmpdir.join("enc.tar.data").strpath
     with open(enc_tar_name, "w+b") as enc_tar:
         enc_tar.write(ciphertext)
         enc_tar.seek(0)
 
-        dfile = DecryptorFile(enc_tar, CONSTANT_TEST_RSA_PRIVATE_KEY)
+        dfile = DecryptorFile(enc_tar, rsa_private_key)
         with tarfile.open(fileobj=cast(IO[bytes], dfile), mode="r") as tar:
             info = tar.getmember("archived_content")
             assert info.isfile() is True
@@ -188,7 +226,7 @@ def test_decryptorfile_for_tarfile(tmpdir: LocalPath) -> None:
                 assert testdata == ext_fp.read()
 
 
-def test_encryptorfile(tmpdir: LocalPath) -> None:
+def test_encryptorfile(tmpdir: LocalPath, rsa_public_key: str, rsa_private_key: str) -> None:
     # create a plaintext blob bigger than IO_BLOCK_SIZE
     plaintext1 = b"rvdmfki6iudmx8bb25tx1sozex3f4u0nm7uba4eibscgda0ckledcydz089qw1p1"
     repeat = int(1.5 * IO_BLOCK_SIZE / len(plaintext1))
@@ -196,7 +234,7 @@ def test_encryptorfile(tmpdir: LocalPath) -> None:
 
     fn = tmpdir.join("data").strpath
     with open(fn, "w+b") as plain_fp:
-        enc_fp = EncryptorFile(plain_fp, CONSTANT_TEST_RSA_PUBLIC_KEY)
+        enc_fp = EncryptorFile(plain_fp, rsa_public_key)
         assert enc_fp.fileno() == plain_fp.fileno()
         assert enc_fp.readable() is False
         with pytest.raises(io.UnsupportedOperation):
@@ -215,7 +253,7 @@ def test_encryptorfile(tmpdir: LocalPath) -> None:
 
         plain_fp.seek(0)
 
-        dec_fp = DecryptorFile(plain_fp, CONSTANT_TEST_RSA_PRIVATE_KEY)
+        dec_fp = DecryptorFile(plain_fp, rsa_private_key)
         assert dec_fp.fileno() == plain_fp.fileno()
         assert dec_fp.readable() is True
         assert dec_fp.seekable() is True
@@ -228,7 +266,7 @@ def test_encryptorfile(tmpdir: LocalPath) -> None:
         assert plaintext == result
 
 
-def test_encryptorfile_for_tarfile(tmpdir: LocalPath) -> None:
+def test_encryptorfile_for_tarfile(tmpdir: LocalPath, rsa_public_key: str, rsa_private_key: str) -> None:
     testdata = b"file contents"
     data_tmp_name = tmpdir.join("plain.data").strpath
     with open(data_tmp_name, mode="wb") as data_tmp:
@@ -236,14 +274,14 @@ def test_encryptorfile_for_tarfile(tmpdir: LocalPath) -> None:
 
     enc_tar_name = tmpdir.join("enc.tar.data").strpath
     with open(enc_tar_name, "w+b") as plain_fp:
-        enc_fp = EncryptorFile(plain_fp, CONSTANT_TEST_RSA_PUBLIC_KEY)
+        enc_fp = EncryptorFile(plain_fp, rsa_public_key)
         with tarfile.open(name="foo", fileobj=cast(IO[bytes], enc_fp), mode="w") as tar:
             tar.add(data_tmp_name, arcname="archived_content")
         enc_fp.close()
 
         plain_fp.seek(0)
 
-        dfile = DecryptorFile(plain_fp, CONSTANT_TEST_RSA_PRIVATE_KEY)
+        dfile = DecryptorFile(plain_fp, rsa_private_key)
         with tarfile.open(fileobj=cast(IO[bytes], dfile), mode="r") as tar:
             info = tar.getmember("archived_content")
             assert info.isfile() is True
@@ -255,13 +293,13 @@ def test_encryptorfile_for_tarfile(tmpdir: LocalPath) -> None:
             assert testdata == content
 
 
-def test_empty_file() -> None:
+def test_empty_file(rsa_public_key: str, rsa_private_key: str) -> None:
     bio = io.BytesIO()
-    ef = EncryptorFile(bio, CONSTANT_TEST_RSA_PUBLIC_KEY)
+    ef = EncryptorFile(bio, rsa_public_key)
     ef.write(b"")
     ef.close()
     assert bio.tell() == 0
 
-    df = DecryptorFile(bio, CONSTANT_TEST_RSA_PRIVATE_KEY)
+    df = DecryptorFile(bio, rsa_private_key)
     data = df.read()
     assert data == b""
