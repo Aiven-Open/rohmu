5bc702c4d8100e9e10c0a41965bb3f9b96bc55bd
Juha Mynttinen <juha.mynttinen@aiven.io>
2025-09-02 06:42:13 +0000

    From refs/heads/main e2b1ac61

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.dev1+g42db9f162',
+    VERSION: '0.1.dev1+ge2b1ac610',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
